043e4646a4f60f9c80b7e15aafa797cae5de470a
Dariusz Pawlukiewicz <goorion@Dariuszs-MacBook-Pro.local>
2021-12-01 14:10:07 +0100

    Added Day 1 Part II

--- src/Day1/Program.cs
+++ src/Day1/Program.cs
@@ -15,4 +15,31 @@ textReports
         return val2;
     });
 
+
 Console.WriteLine(counter);
+
+
+//========= PART 2 ===========
+
+var counterGroup = 0;
+
+var numbers = textReports
+    .Split(Environment.NewLine)
+    .Where(t => t != string.Empty)
+    .Select(t => Convert.ToInt32(t))
+    .ToList();
+
+var group = new List<(int, int, int)>();
+
+for (var i = 0; i < numbers.Count - 3; i++)
+{
+    var sumLeft = numbers[i] + numbers[i+1] + numbers[i+2];
+    var sumRight = numbers[i+1] + numbers[i+2] + numbers[i+3];
+
+    if (sumLeft < sumRight)
+    {
+        counterGroup++;
+    }
+}
+
+Console.WriteLine(counterGroup);
