14362bd5a5a8df970edfb14ed1c9efc525345ce1
Dariusz Pawlukiewicz <goorion@Dariuszs-MacBook-Pro.local>
2021-12-01 13:09:27 +0100

    Added Day 1 solution

---
+++ src/Day1/Program.cs
@@ -0,0 +1,18 @@
+﻿var textReports =  File.ReadAllText(@"input.txt");
+var counter = 0;
+
+textReports
+    .Split(Environment.NewLine)
+    .Where(t => t != string.Empty)
+    .Select(t => Convert.ToInt32(t))
+    .Aggregate((val1, val2) =>
+    {
+        if (val1 < val2)
+        {
+            counter++;
+        }
+
+        return val2;
+    });
+
+Console.WriteLine(counter);
